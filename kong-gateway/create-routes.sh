e7523e70ec3840faf61c8e6584ad3a3b1c3cf7f5
Joe Dascole <joe.dascole@konghq.com>
2023-01-19 08:28:29 -0500

    FIX add variablized protocol

--- kong-gateway/create-routes.sh
+++ kong-gateway/create-routes.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+PROTO=https
 SERVICE_NAME=httpbin
 HOST=localhost
 PORT=8001
 TOKEN=kong
 
 # Create a service
-curl -H "kong-admin-token: $TOKEN" -i -X POST https://$HOST:$PORT/services \
+curl -H "kong-admin-token: $TOKEN" -i -X POST $PROTO://$HOST:$PORT/services \
   --data name=$SERVICE_NAME \
   --data url='http://httpbin.org/anything' -k
 
 # Create a ton of routes
 for i in {1..100}
 do
-  curl -s http://$HOST:$PORT/services/$SERVICE_NAME/routes -H "kong-admin-token: $TOKEN" --header "Content-Type: application/x-www-form-urlencoded" \
-  --data-urlencode 'name=path'${i} --data-urlencode 'paths=/path'${i} --data-urlencode 'hosts=hosts'${i} --insecure -o /dev/null
+  curl -s $PROTO://$HOST:$PORT/services/$SERVICE_NAME/routes -H "kong-admin-token: $TOKEN" --header "Content-Type: application/x-www-form-urlencoded" \
+  --data-urlencode 'name=path'${i} --data-urlencode 'paths=/path'${i} --insecure -o /dev/null
 done
